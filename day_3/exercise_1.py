b8f7954655b6b14597874db98508b66495ab881c
jeppe <jeppe.r.a321@gmail.com>
2022-06-03 15:13:34 +0200

    update

---
+++ day_3/exercise_1.py
@@ -0,0 +1,11 @@
+'''
+Write a program that works out whether if a given number is an odd or even number.
+'''
+
+num = int(input('Enter a number: '))
+
+if num % 2 == 0:
+    print(f'{num} is an even number')
+
+else:
+    print(f'{num} is an odd number')
